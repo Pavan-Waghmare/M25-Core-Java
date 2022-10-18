8b07549c8cdfd32368a4ac5391ed2dd6aadbcf3e
admin <admin@DESKTOP-S6OFAAK>
2022-10-18 09:48:28 +0530

    Assign_1

---
+++ Assignment1/src/org/tnsindia/operator/TernoryOperato.java
@@ -0,0 +1,14 @@
+package org.tnsindia.operator;
+
+//Example on Ternory Operator
+public class TernoryOperato {
+
+	public static void main(String[] args) {
+		int a=4;
+		int b=5;
+		int res =(a>b)? a:b;
+		System.out.println(res);
+
+	}
+
+}
