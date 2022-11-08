ab70bcb556fe07a89d544baeaf041a70e9090b15
admin <admin@DESKTOP-S6OFAAK>
2022-11-08 21:18:24 +0530

    Day22

---
+++ Day22_GoShopping/src/org/tnsindia/appication/GSNormalAcc.java
@@ -0,0 +1,18 @@
+package org.tnsindia.appication;
+
+import org.tnsindia.framework.NormalAcc;
+
+public class GSNormalAcc extends NormalAcc{
+
+	public GSNormalAcc(int accNo, String accNm, float charges, float deliveryCharges) {
+		super(accNo, accNm, charges, deliveryCharges);
+		
+	}
+
+	@Override
+	public String toString() {
+		return "GSNormalAcc []";
+	}
+	
+
+}
