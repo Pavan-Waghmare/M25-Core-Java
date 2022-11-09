dc4e33156fce85b5f24c51613e0343a347603d79
admin <admin@DESKTOP-S6OFAAK>
2022-11-09 10:46:55 +0530

    First Commit

---
+++ Day-23_BankingApplication/src/org/tnsindia/framework/CurrentAccount.java
@@ -0,0 +1,23 @@
+package org.tnsindia.framework;
+
+import org.tnsindia.framework.BankAccount;
+
+public abstract class CurrentAccount extends BankAccount 
+{
+	
+	private final float creditLimit;
+	public CurrentAccount(int accNo, String accNm, float accBal,float creditLimit) {
+		super(accNo, accNm, accBal);
+		this.creditLimit=creditLimit;
+	}
+	public void withdraw (float accBal) 
+	{
+		System.out.println("Account No is:"+this.getAccNo()+"Account Name is:"+this.getAccNm()+
+				"Account Balance is:"+(accBal + creditLimit));
+	}
+	@Override
+	public String toString() {
+		return String.format("CurrentAccount [creditLimit=%s]", creditLimit);
+	}
+
+}
